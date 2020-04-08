e12e6f6748e966fc37bcd4438cac13ee848e2b5f
Verdungo <q>
2020-04-08 23:05:47 +0500

    5. Filter Operator

---
+++ src/main/java/operators/filtering/FilterOperator.java
@@ -0,0 +1,19 @@
+package operators.filtering;
+
+import io.reactivex.Observable;
+import io.reactivex.functions.Predicate;
+import models.Shape;
+import observer.DemoObserver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import utils.RxUtils;
+
+public class FilterOperator {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TakeOperator.class);
+
+    public static void main(String[] args) {
+        Observable.fromIterable(RxUtils.shapes(30))
+                .filter(shape -> "green".equals(shape.getColor()) && "square".equals(shape.getShape()))
+                .subscribe(new DemoObserver<>());
+    }
+}
